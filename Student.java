4b58ff17a3b65bb2be5f31f7c6c1b54dd50eccb7
dongtianshuo <72388152+dongtianshuo@users.noreply.github.com>
2020-11-03 14:58:17 +0800

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,6 @@
+package Shiyan4;
+
+interface Student {
+    int jiao(int x1);
+    int cha(int x2);
+}
---
+++ Teacher.java
@@ -0,0 +1,6 @@
+package Shiyan4;
+
+public interface Teacher {
+    double fa(double x3);
+    double chaxin(double x4);
+}
